6e8f4937a6d4cb132a4ba18198ca40048dd914e1
jbh105 <jaydipbhayani105@gmail.com>
2023-07-03 16:09:51 +0530

    add new keywords

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,20 +8,129 @@ export default function Document() {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="URL Master" />
         <meta property="og:image" content="/assets/logo.png" />
-        <meta name="og:description" content="This is URL master Web App" />
-        <meta name="keywords" content="name,HTML" />
-        <style>
-          {`
-          img.og-image {
-            width: 300px; 
-            height: auto;
-            border: 2px solid black; 
-          }
-          `}
-        </style>
+        <meta
+          name="og:description"
+          content="Instantly convert any file type to a live URL. Effortlessly transform and share documents, images, videos, and more. Try our file-to-live-URL converter now!"
+        />
+        <meta
+          name="keywords"
+          content="url,
+          pdf,
+          link,
+          live link,
+          pdf to live link,
+          pdf to link, 
+          doc to link, 
+          docs to link, 
+          image to link,
+          image live link, 
+          image to link, 
+          doc to link, 
+          docs to link, 
+          pdf to link,
+          File converter,
+          Live URL generator,
+          Document conversion,
+          Image conversion,
+          Video conversion,
+          Audio conversion,
+          Presentation conversion,
+          Spreadsheet conversion,
+          PDF conversion,
+          Word to PDF,
+          Image to PDF,
+          Video to GIF,
+          File sharing,
+          Online tool,
+          Instant conversion,
+          Seamless file conversion,
+          Effortless sharing,
+          Shareable links,
+          Collaborative workflow,
+          Convert to URL,
+          File format transformation,
+          Convert documents online,
+          File to link converter,
+          URL generator tool,
+          Share files easily,
+          Convert to web link,
+          Link creator,
+          Convert and share files,
+          Online document converter,
+          Image to URL,
+          Video to URL,
+          Document to URL,
+          Bulk file conversion,
+          URL shortener,
+          URL generator online,
+          URL converter,
+          File conversion made easy,
+          Convert to clickable link,
+          URL creator tool,
+          Quick file conversion,
+          Image hosting,
+          Embed files in URLs,
+          Convert and share with ease,
+          Online file transformation,
+          Convert files to live URLs,
+          Generate shareable URLs,
+          Convert files on the fly,
+          Convert to web-ready formats,
+          Online URL generator,
+          Easy file conversion,
+          Image to shareable link,
+          Document to shareable link,
+          Video to shareable link,
+          Convert files in seconds,
+          URL sharing made simple,
+          Convert to public URL,
+          Convert files to accessible links,
+          Convert files to HTML links,
+          Convert files for online sharing,
+          Link generator tool,
+          Share documents as URLs,
+          Convert files for collaboration,
+          Online media converter,
+          Convert files to online content,
+          Instantly share files as URLs,
+          File converter with URL output,
+          Convert files for website integration,
+          File to web link converter,
+          Securely convert files to URLs,
+          Convert files for social media sharing,
+          Convert files for email sharing,
+          Convert files for online publishing,
+          Convert files for cloud storage,
+          Convert files for online presentations,
+          Share files across platforms,
+          Convert files for online viewing,
+          Convert files for web embedding,
+          Convert files for online marketing,
+          Convert files for e-commerce,
+          Convert files for content management systems,
+          Convert files for online collaboration tools,
+          Convert files for project management platforms,
+          Convert files for learning management systems,
+          Convert files for customer relationship management systems,
+          Convert files for document management systems,
+          Convert files for online surveys,
+          Convert files for webinars,
+          Convert files for online training courses,
+          Convert files for virtual events,
+          Convert files for online portfolios,
+          Convert files for online resumes,
+          Convert files for online job applications,
+          Convert files for online forms,
+          Convert files for online catalogs,
+          Convert files for online brochures,
+          Convert files for online newsletters,
+          Convert files for online magazines,
+          Convert files for online newspapers,
+          Convert files for online books,
+          Convert files for online tutorials"
+        />
       </Head>
       <body>
-        <img className="og-image" src="/assets/logo.png" alt="Logo" />
         <Main />
         <NextScript />
       </body>

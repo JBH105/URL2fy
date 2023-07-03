0ad71cfb8d0e49612dd6b594b6c533def4adad6a
jbh105 <jaydipbhayani105@gmail.com>
2023-07-03 14:18:14 +0530

    fixed the head color

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="theme-color" content="#000000" />
+        {/* <meta name="theme-color" content="#000000" /> */}
         <meta property="og:title" content="URL Master" />
         <meta property="og:image" content="/assets/logo.png" />
         <meta name="og:description" content="This is URL master Web App" />

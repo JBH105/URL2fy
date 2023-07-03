09b0b689a87e4b9536bf5bcb7c756238fbc3672b
jbh105 <jaydipbhayani105@gmail.com>
2023-07-03 15:05:26 +0530

    fixed env file issue

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-require('dotenv').config()
 const nextConfig = {
   reactStrictMode: true,
-  // env: {
-  //   BUCKETNAME:"",
-  //   ACCESSKEYID:"",
-  //   SECRETACCESSKEY:""
-  // },
 };
 
 module.exports = nextConfig;

1a4a9b3deb7b647eb4016e03e613258c22dd4b7a
jbh105 <jaydipbhayani105@gmail.com>
2023-07-03 15:52:50 +0530

    fixed the image issue in meta tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,12 +5,23 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* <meta name="theme-color" content="#000000" /> */}
+        <meta property="og:type" content="website" />
         <meta property="og:title" content="URL Master" />
         <meta property="og:image" content="/assets/logo.png" />
         <meta name="og:description" content="This is URL master Web App" />
         <meta name="keywords" content="name,HTML" />
+        <style>
+          {`
+          img.og-image {
+            width: 300px; 
+            height: auto;
+            border: 2px solid black; 
+          }
+          `}
+        </style>
       </Head>
       <body>
+        <img className="og-image" src="/assets/logo.png" alt="Logo" />
         <Main />
         <NextScript />
       </body>
